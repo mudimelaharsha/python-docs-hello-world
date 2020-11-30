399205abffd90ca00efe3fed78ebdf3e7cd71179
milindvb <mibelhe@microsoft.com>
2020-11-30 17:19:16 -0600

    Update application.py

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def hello():
     database = 'test' 
     username = 'milindvb' 
     password = 'test123456789###' 
-    table = '
+
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
 
